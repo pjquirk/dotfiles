8fba0a78f7adc177ba071ef6251bdbb13577213d
PJ Quirk <pjquirk@github.com>
2021-09-08 15:36:30 -0400

    Collapse the home dir

---
+++ home/.config/powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,7 @@
+# Uncomment one of these based on your org/team
+$env:AzureSubscriptionId="8a53cb9a-a3a5-4602-aa2d-8c171edde3c7" # John Mogensen's org (Actions_Platform_Eng)
+# $env:AzureSubscriptionId="16eb6e57-e88b-49c9-8acb-26048bee1f93" # c2c-actions-compute specifically (VCFP_Eng)
+# $env:AzureSubscriptionId="ed693d19-8167-4d94-9193-b21025975b8f" # Youhana's org (Actions_Core_Eng)
+
+# Set some POSH Git settings
+$GitPromptSettings.DefaultPromptAbbreviateHomeDirectory = $true
