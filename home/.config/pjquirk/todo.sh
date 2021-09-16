01083a2848a9e76e919cd611920cf293f3562021
PJ Quirk <pjquirk@github.com>
2021-09-16 16:33:21 -0400

    Changed to have subcommands

--- home/.config/pjquirk/todo.sh
+++ home/.config/pjquirk/todo.sh
@@ -1,68 +1,119 @@
 #!/usr/env/var bash
 # shellcheck shell=bash
 
-tdhelp () {
-  echo 'ToDo:'
-  echo '  todo                List open items'
-  echo '  tdn "<title>"       Create a new issue'
-  echo '  tdc <number>        Close an existing issue'
-  echo '  tdo <number>        Open an issue for notes'
-  echo '  tdl <issue URI>     Open a note issue to track an existing issue'
+CLI_NAME='todo'
+
+todo_help () {
+  echo "ToDo Tracking with gh CLI
+
+Usage: ${CLI_NAME} [command]
+
+Commands:
+  <no arg>          Displays current list of issues
+  new (create)      Creates a new issue in the linked repository
+  close             Closes an existing issue in the linked repository
+  ref               Creates a new issue to track a different given issue
+  open (view)       Opens the given issue in a browser
+  help              Displays this message
+"
+  return 1
 }
 
 gh_repo () {
   gh --repo pjquirk/worknotes "$@"
 }
 
-todo () {
+todo_list () {
   gh_repo issue list --assignee @me
 }
 
-tdn () {
+todo_new () {
   IFS=' ' # make sure "$*" is joined with spaces
   # Glob all arguments into a string
   local title="$*"
   if [[ -z "${title}" ]]; then
     echo \
-"Expected a title for the issue, e.g.:
-  ${FUNCNAME[0]} A pretty standard title
-  ${FUNCNAME[0]} \"A title that contains an ' or \" character\"" >&2
+"Creates a new issue in the linked repository.
+
+Usage: ${CLI_NAME} new <title>
+Examples:
+  ${CLI_NAME} new A pretty standard title
+  ${CLI_NAME} new \"A title that contains an ' or \" character\"" >&2
     return 1
   fi
   gh_repo issue create --assignee @me --body "" --title "${title}"
 }
 
-tdc () {
+todo_close () {
   local number="${1}"
   if ! [[ $number =~ ^[0-9]+$ ]] ; then
    echo \
-"Expected an issue number to close, e.g.:
-  ${FUNCNAME[0]} 123" >&2
+"Closes an existing issue in the linked repository.
+
+Usage: ${CLI_NAME} close <issue number>
+Examples:
+  ${CLI_NAME} close 123" >&2
    return 1
   fi
   gh_repo issue close "${number}"
 }
 
-tdo () {
+todo_open () {
   local number="${1}"
   if ! [[ $number =~ ^[0-9]+$ ]] ; then
-   echo \
-"Expected an issue number to open, e.g.:
-  ${FUNCNAME[0]} 123" >&2
+    echo \
+"Opens an issue in a browser.
+
+Usage: ${CLI_NAME} open <issue number>
+Examples:
+  ${CLI_NAME} open 123" >&2
    return 1
   fi
   gh_repo issue view "${number}" --web
 }
 
-tdl () {
+todo_ref () {
   local uri="$1"
   if [[ -z "${uri}" ]]; then
     echo \
-"Expected a URL of an issue to link, e.g.:
-  ${FUNCNAME[0]} https://github.com/owner/repo/issues/1" >&2
+"Creates a new issue to track a different given issue.
+
+Usage: ${CLI_NAME} ref <issue URI>
+Examples:
+  ${CLI_NAME} ref https://github.com/owner/repo/issues/1" >&2
     return 1
   fi
   local issue_title
   issue_title=$(gh issue view "${uri}" --json title --jq '.title')
   gh_repo issue create --assignee @me --body "Notes for ${uri}" --title "${issue_title}"
+}
+
+todo () {
+  local command="$1"
+  if [[ -z "$command" ]]; then
+    todo_list
+  else
+    shift # Remove the command from the list of args
+    case "$command" in
+      new|create)
+        todo_new "$@"
+        ;;
+      close)
+        todo_close "$@"
+        ;;
+      ref)
+        todo_ref "$@"
+        ;;
+      open|view)
+        todo_open "$@"
+        ;;
+      help)
+        todo_help
+        ;;
+      *)
+        echo "Unknown command: $command" >&2
+        todo_help
+        ;;
+    esac
+  fi
 }
