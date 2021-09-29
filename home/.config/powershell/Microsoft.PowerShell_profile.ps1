53ee74c084a10146092712494699b3c778c6d64c
PJ Quirk <pjquirk@github.com>
2021-09-29 19:36:04 +0000

    Add azure region

--- home/.config/powershell/Microsoft.PowerShell_profile.ps1
+++ home/.config/powershell/Microsoft.PowerShell_profile.ps1
@@ -7,6 +7,7 @@ Import-Module posh-git
 $env:AzureSubscriptionId="8a53cb9a-a3a5-4602-aa2d-8c171edde3c7" # John Mogensen's org (Actions_Platform_Eng)
 # $env:AzureSubscriptionId="16eb6e57-e88b-49c9-8acb-26048bee1f93" # c2c-actions-compute specifically (VCFP_Eng)
 # $env:AzureSubscriptionId="ed693d19-8167-4d94-9193-b21025975b8f" # Youhana's org (Actions_Core_Eng)
+$env:AzureRegion = "eastus"
 
 # Set some POSH Git settings
 $GitPromptSettings.DefaultPromptAbbreviateHomeDirectory = $true
