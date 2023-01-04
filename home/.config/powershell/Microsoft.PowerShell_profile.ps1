8fa0974538cfce5cab186b71a898e80b94a59f51
PJ Quirk <pjquirk@github.com>
2023-01-04 11:05:06 -0500

    Use the VCFP sub

--- home/.config/powershell/Microsoft.PowerShell_profile.ps1
+++ home/.config/powershell/Microsoft.PowerShell_profile.ps1
@@ -7,8 +7,8 @@ if (-not (Get-InstalledModule -ErrorAction Ignore -Name posh-git)) {
 Import-Module posh-git
 
 # Uncomment one of these based on your org/team
-$env:AzureSubscriptionId="8a53cb9a-a3a5-4602-aa2d-8c171edde3c7" # John Mogensen's org (Actions_Platform_Eng)
-# $env:AzureSubscriptionId="16eb6e57-e88b-49c9-8acb-26048bee1f93" # c2c-actions-compute specifically (VCFP_Eng)
+# $env:AzureSubscriptionId="8a53cb9a-a3a5-4602-aa2d-8c171edde3c7" # John Mogensen's org (Actions_Platform_Eng)
+$env:AzureSubscriptionId="16eb6e57-e88b-49c9-8acb-26048bee1f93" # c2c-actions-compute specifically (VCFP_Eng)
 # $env:AzureSubscriptionId="ed693d19-8167-4d94-9193-b21025975b8f" # Youhana's org (Actions_Core_Eng)
 $env:AzureRegion = "eastus"
 
@@ -17,7 +17,7 @@ $GitPromptSettings.DefaultPromptAbbreviateHomeDirectory = $true
 $GitPromptSettings.DefaultPromptPrefix.Text = '$(if ($env:SKYRISEV3) { "Skyrise " } else { "" })'
 $GitPromptSettings.DefaultPromptPrefix.ForegroundColor = [ConsoleColor]::Magenta
 
-# 
+#
 # Helper functions
 #
 
@@ -26,7 +26,7 @@ function New-Tracepoint {
     {
         # Truncate to get last two digits equal to zero
         $tp = Get-Random -Minimum 10000 -Maximum 21474836
-        # Search for a commonly used pattern since we can't search for partial tracepoints 
+        # Search for a commonly used pattern since we can't search for partial tracepoints
         $testTracepoint = '{0}01' -f $tp
         # Check if it exists in the source already
         $count = & gh api -H "Accept: application/vnd.github.v3+json" "/search/code?q=$testTracepoint%20repo:github/actions-dotnet%20extension:cs&per_page=1" |
